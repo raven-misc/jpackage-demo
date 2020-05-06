bf111df605d02b088fdbc21aab1455a684a7fa89
Raven <zhang.xingrui@foxmail.com>
2020-05-06 10:43:57 +0800

    提交模块和非模块程序源码

---
+++ Non-modular-packaging-demo/src/com/raven/App.java
@@ -0,0 +1,14 @@
+package com.raven;
+
+import java.awt.*;
+
+/**
+ * @author Raven
+ * @version 1.0
+ * @date 2020/5/6 8:53
+ */
+public class App {
+    public static void main(String[] args) {
+        Toolkit.getDefaultToolkit().beep();
+    }
+}
